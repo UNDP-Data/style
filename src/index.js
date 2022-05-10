b338233718811a150ba812f2a76df2092545e4fd
Jin Igarashi <igarashi.jin@water-gis.com>
2022-05-10 18:24:15 +0100

    fixed sprite

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const config = require("./config");
 
 const example = () => {
   console.time("sprite-create");
-  const spriteCreator = new SpriteCreator(true);
+  const spriteCreator = new SpriteCreator();
   spriteCreator.build(config.sprites);
   console.timeEnd("sprite-create");
 };
--- src/sprite-creator.js
+++ src/sprite-creator.js
@@ -1,17 +1,12 @@
-const spritezero = require("@elastic/spritezero");
+const spritezero = require("@mapbox/spritezero");
 const fs = require("fs");
 const glob = require("glob");
 const path = require("path");
 const rimraf = require("rimraf");
 
 class SpriteCreator {
-  isSDF = false;
 
-  constructor(isSDF) {
-    if (isSDF && isSDF === true) {
-      this.isSDF = isSDF;
-    }
-  }
+  constructor() {}
 
   build(sprites) {
     sprites.forEach((s) => {
@@ -66,9 +61,12 @@ class SpriteCreator {
     // Pass `true` in the layout parameter to generate a data layout
     // suitable for exporting to a JSON sprite manifest file.
     spritezero.generateLayout(
-      { imgs: svgs, pixelRatio: pxRatio, sdf: this.isSDF, format: true },
+      { imgs: svgs, pixelRatio: pxRatio, format: true },
       function (err, dataLayout) {
         if (err) return;
+        Object.keys(dataLayout).forEach(key=>{
+          dataLayout[key].sdf = true
+        })
         fs.writeFileSync(jsonPath, JSON.stringify(dataLayout, null, 2));
       }
     );
@@ -76,7 +74,7 @@ class SpriteCreator {
     // Pass `false` in the layout parameter to generate an image layout
     // suitable for exporting to a PNG sprite image file.
     spritezero.generateLayout(
-      { imgs: svgs, pixelRatio: pxRatio, sdf: this.isSDF, format: false },
+      { imgs: svgs, pixelRatio: pxRatio, format: false },
       function (err, imageLayout) {
         if (err) return;
         spritezero.generateImage(imageLayout, function (err, image) {
